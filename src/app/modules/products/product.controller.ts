6141491adb8e4378ac17d1ddbb1e63b36cb44b0a
Priyanka-Das-Dipa <priyankadipa9102@gmail.com>
2024-05-23 17:03:49 +0600

    [get data by Id] write the api in controller

--- src/app/modules/products/product.controller.ts
+++ src/app/modules/products/product.controller.ts
@@ -2,6 +2,8 @@ import { Request, Response } from 'express';
 import ProductZodValidationSchema from './product.validation';
 import { productServices } from './product.service';
 
+
+// create new data to the database
 const createProduct = async (req: Request, res: Response) => {
   try {
     const productData = req.body;
@@ -22,6 +24,7 @@ const createProduct = async (req: Request, res: Response) => {
 };
 
 
+// get all the data from the database
 const getAllProduct = async (req: Request, res: Response) =>{
   try {
     const result = await productServices.getAllProducts();
@@ -41,10 +44,36 @@ const getAllProduct = async (req: Request, res: Response) =>{
 }
 
 
+// get the specific product by ID
+const getProductByID = async (req : Request, res: Response) =>{
+    try {
+      const id = req.params.productID;
+      const result = await productServices.getOneProductById(id);
+
+      res.status(200).json({
+        success: true,
+        message: "Specified Id product data fetch successfully!",
+        data: result,
+      });
+      
+    } catch (error : any) {
+      res.status(500).json({
+        success: false,
+        message: "Something went wrong",
+        error: error.issues.map((item: { message: unknown }) => item.message) || "Internal Server Error",
+      });
+    }
+}
+
+
+
+
+
 
 export const productsController = {
     createProduct,
-    getAllProduct
+    getAllProduct,
+    getProductByID
 }
 
 
